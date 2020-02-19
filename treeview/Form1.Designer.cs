0b4abc657b6bf4ae1ab3c717ea8096c8fc3c7167
ripskail <rip-1995@mail.ru>
2020-02-19 10:19:46 +0500

    1
    1

---
+++ treeview/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace treeview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.treeView1 = new System.Windows.Forms.TreeView();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.tableLayoutPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.Controls.Add(this.splitContainer1, 0, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 3;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 51F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(635, 333);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.button4);
+			this.splitContainer1.Panel1.Controls.Add(this.button1);
+			this.splitContainer1.Panel1.Controls.Add(this.button3);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.treeView1);
+			this.tableLayoutPanel1.SetRowSpan(this.splitContainer1, 3);
+			this.splitContainer1.Size = new System.Drawing.Size(609, 327);
+			this.splitContainer1.SplitterDistance = 200;
+			this.splitContainer1.TabIndex = 0;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(0, 103);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(138, 43);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "save";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4_Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(0, 3);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(139, 43);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Open";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(0, 52);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(138, 45);
+			this.button3.TabIndex = 0;
+			this.button3.Text = "Sort";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3_Click);
+			// 
+			// treeView1
+			// 
+			this.treeView1.Location = new System.Drawing.Point(3, 0);
+			this.treeView1.Name = "treeView1";
+			this.treeView1.Size = new System.Drawing.Size(417, 332);
+			this.treeView1.TabIndex = 0;
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.OpenFileDialog1_FileOk);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(635, 333);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.TreeView treeView1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+	}
+}
+
---
+++ treeview/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace treeview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+			if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+				return;
+			// получаем выбранный файл
+			string filename = openFileDialog1.FileName;
+			// читаем файл в строку
+			//string fileText = System.IO.File.ReadAllText(filename);
+			OpenFilesToTreeView(filename, treeView1);
+        }
+
+        public void OpenFilesToTreeView (string NameFiles, TreeView treeview)
+        {
+            string lineFile;
+            string[] tmplineFile;
+            FileStream file1 = new FileStream(NameFiles, FileMode.Open);
+            using (StreamReader readerFiles = new StreamReader(file1))
+            {
+                while ((lineFile = readerFiles.ReadLine()) != null)
+                {
+                    tmplineFile = new[] { lineFile };
+                    PopulateTreeView(treeView1, tmplineFile);
+                }
+            }
+        }
+
+        void PopulateTreeView(TreeView treeView, string[] paths)
+        {
+            TreeNode lastNode = null;
+            TreeNode[] nodes = new TreeNode[0];
+            string subPathAgg;
+            foreach (string path in paths)
+            {
+                subPathAgg = string.Empty;
+                foreach (string subPath in path.Split('\\'))
+                {
+                    string[] temporary = path.Split('\\');
+                    
+                        subPathAgg += subPath + "\\";
+                        nodes = treeView.Nodes.Find(subPathAgg, true);
+                        if (nodes.Length == 0)
+                            if (lastNode == null)
+                                lastNode = treeView.Nodes.Add(subPathAgg, subPath, 0);
+                            else
+                                lastNode = lastNode.Nodes.Add(subPathAgg, subPath, 0);
+                        else
+                            lastNode = nodes[0];
+                    
+                }
+            }
+            lastNode = null;
+            
+        }
+      public class MySortClass : IComparer
+        {
+ 
+            public int Compare(object _left, object _right)
+            {
+                TreeNode left = _left  as TreeNode;
+                TreeNode right = _right as TreeNode;
+
+				Regex leftRegex = new Regex(@"[a-z].*[0-9]*");
+				Match leftMatch = leftRegex.Match(left.Text);
+
+				Regex rightRegex = new Regex(@"[a-z].*[0-9]*");
+				Match rightMatch = leftRegex.Match(right.Text);
+
+				string lleftMatch = leftRegex.Match(left.Text).ToString();
+				string rrightMatch = rightRegex.Match(right.Text).ToString();
+				int value1;
+				int.TryParse(string.Join("", lleftMatch.Where(c => char.IsDigit(c))), out value1);
+				int value2;
+				int.TryParse(string.Join("", rrightMatch.Where(c => char.IsDigit(c))), out value2);
+				if (leftMatch.Success || rightMatch.Success)
+				{
+					if (leftMatch.Success && rightMatch.Success)
+					{
+						if (value1 > value2)
+						{
+							return 0;
+						}
+						if (value1 == value2)
+						{
+							return string.Compare(left.Text, right.Text);
+						}
+							return 1;
+					}
+			
+					if (value1 > value2)
+					{
+						return 1;
+					}
+					return 0;
+				}
+
+				else
+				{
+
+					if (string.Compare(left.Text, right.Text) >= 0)
+					{
+						return 1;
+					}
+					else
+					{
+
+						return 0;
+
+					}
+				} 
+            }
+        }
+
+		private void PrintRecursive(TreeNode treeNode,string filename)
+		{
+			//System.Diagnostics.Debug.WriteLine(treeNode.Text);
+			//MessageBox.Show(treeNode.Text+ " " + treeNode.Parent + " " + treeNode.Level);
+			int lev = treeNode.Level;
+			string path = filename;
+			
+			if (File.Exists(path))
+			{
+				string s = treeNode.Text.Insert(0, new string(' ', lev));
+				File.AppendAllText(path, s + Environment.NewLine, Encoding.UTF8);
+				//sw.WriteLine(treeNode.Text.PadLeft(lev, pad));
+			}
+			else {
+				FileStream file1 = new FileStream(path, FileMode.OpenOrCreate);
+				file1.Close();
+			}
+				foreach (TreeNode tn in treeNode.Nodes)
+			{
+				PrintRecursive(tn, filename);
+			}
+		
+		}
+
+		private void CallRecursive(TreeView treeView)
+		{
+			if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+				return;
+			// получаем выбранный файл
+			string filename = saveFileDialog1.FileName;
+			TreeNodeCollection nodes = treeView.Nodes;
+			foreach (TreeNode n in nodes)
+			{
+				PrintRecursive(n, filename);
+			}
+		}
+		private void Button3_Click(object sender, EventArgs e)
+        {
+			treeView1.TreeViewNodeSorter = new MySortClass();
+
+		}
+		private void Button4_Click(object sender, EventArgs e)
+		{
+			CallRecursive(treeView1);
+		}
+	}
+}
